8e034246f166a68b519b7027ee9248747b16f809
Ahsan Ateeq <ahsan.ateeq1@outlook.com>
2019-12-06 16:10:03 +0500

    Initial Commit

---
+++ ScalingCarousel/ScalingCarousel.h
@@ -0,0 +1,19 @@
+//
+//  ScalingCarousel.h
+//  ScalingCarousel
+//
+//  Created by Ahsan Ateeq on 06/12/2019.
+//  Copyright © 2019 Proficeint Tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScalingCarousel.
+FOUNDATION_EXPORT double ScalingCarouselVersionNumber;
+
+//! Project version string for ScalingCarousel.
+FOUNDATION_EXPORT const unsigned char ScalingCarouselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScalingCarousel/PublicHeader.h>
+
+
