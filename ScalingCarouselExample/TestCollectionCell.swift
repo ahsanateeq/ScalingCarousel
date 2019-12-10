fbd38370f3c3b13d17711f871c88687c9f9b05e5
Ahsan Ateeq <ahsanateeq_iosdev@icloud.com>
2019-12-10 12:52:59 +0500

    adding example for Scaling Carousel

---
+++ ScalingCarouselExample/TestCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  TestCollectionCell.swift
+//  ScalingCarouselExample
+//
+//  Created by Ahsan Ateeq on 10/12/2019.
+//  Copyright © 2019 Proficeint Tech. All rights reserved.
+//
+
+import UIKit
+import ScalingCarousel
+class TestCollectionCell: ScalingCarouselCVCell {
+    
+}
---
+++ ScalingCarouselExample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ScalingCarouselExample
+//
+//  Created by Ahsan Ateeq on 10/12/2019.
+//  Copyright © 2019 Proficeint Tech. All rights reserved.
+//
+
+import UIKit
+import ScalingCarousel
+
+class ViewController: ScalingCarouselCollectionVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
